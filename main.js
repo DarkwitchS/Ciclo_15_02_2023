6472f2a4e9ba9dbf4621fcddb8d979edbdf1af96
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-15 20:32:30 -0500

    Ciclo for

---
+++ main.js
@@ -0,0 +1,11 @@
+let i = 0;
+let bandera = true;
+for(i; bandera;){
+    console.log(bandera);
+    bandera = (i<5) ? ++i: false;
+}
+
+
+// for (let i = 0; i <=5; i++) {
+//     console.log(i);
+// }
